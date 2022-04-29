6ca82742cfd61c424090e2fd356f05e9bf59cbe4
maxbre <maxbre67@gmail.com>
2022-04-29 14:22:32 +0000

    initial commit

--- R/mapview_calpuff_setup_qafiles.R
+++ R/mapview_calpuff_setup_qafiles.R
@@ -80,7 +80,7 @@ mapview_emi_points <- function(qapnts.dat, epsg = 32632, string_filename = 'file
   emi_pts_sf <- read_qapnts_dat(qapnts.dat, epsg = epsg)
 
   # mapview
-  map <- mapview::mapview(emi_pts_sf, color="red", layer.name = name_of_map_layer)
+  map <- mapview::mapview(emi_pts_sf, layer.name = name_of_map_layer)
 
   export_mapview(map, string_filename)
 
@@ -102,7 +102,7 @@ mapview_disc_recs <- function(qarecd.dat, epsg = 32632, string_filename = 'file_
   disc_recs_sf <- read_qarecd_dat(qarecd.dat, epsg = epsg)
 
   # mapview
-  map <- mapview::mapview(disc_recs_sf, color="red", layer.name = name_of_map_layer)
+  map <- mapview::mapview(disc_recs_sf, layer.name = name_of_map_layer)
 
   export_mapview(map, string_filename)
 
@@ -124,10 +124,45 @@ mapview_grid_recs <- function(qarecg.dat, epsg = 32632, string_filename = 'file_
   grid_recs_sf <- read_qarecg_dat(qarecg.dat, epsg = epsg)
 
   # mapview
-  map <- mapview::mapview(grid_recs_sf, color="red", layer.name = name_of_map_layer)
+  map <- mapview::mapview(grid_recs_sf, layer.name = name_of_map_layer)
 
   export_mapview(map, string_filename)
 
 }
 
-# here goes the code for the mapview qaterr_grd, recycle from previous scripts mapview_calpost_grd.R
+#' mapview_qaterr_grd
+#'
+#' Create a mapview object from Calpuff setup grid file qaterr.grd and export as html and png files
+#'
+#' @param file_grd string path to the Calpuff setup grid file qaterr.grd
+#' @param epsg number epsg to set crs in the raster object
+#' @param string_filename string to name the output files
+#' @param name_of_map_layer string to name the mapview layer
+#' @return mapview object exported as html and png files
+#' @export
+
+mapview_calpuff_terr <- function(file_grd, epsg = 32632, string_filename = ' file_name_grid_terr', name_of_map_layer = 'layer_name_grid_terr'){
+
+  grd <- read_qaterr_grd(file_grd, epsg)
+
+  # eventually to hardcode the binning in mapview
+  #my_bins<-c(0, 1, 2, 3, 4, 5, 10, 100, round(max(raster::values(grd)),0))
+  #my_bins<-pretty(range(raster::values(grd)))
+
+  # mapview
+  map <- mapview::mapview(grd,
+                          # define the palette
+                          col.regions = grDevices::colorRampPalette(rev(RColorBrewer::brewer.pal(8, 'RdYlBu'))),
+                          # here eventually define the binning
+                          #at = my_bins,
+                          na.color ='transparent',
+                          alpha.region= 0.5,
+                          legend.opacity=0.5,
+                          layer.name = name_of_map_layer)
+
+
+  export_mapview(map, string_filename)
+
+}
+
+
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
 # rcalpuff
-my functions
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+A collection of my handy R functions for the Calpuff modeling system.
+
+Beware: all the material must be considered experimental, in full
+development, not yet tested.
+
+Caveat emptor: use it at your own risk.
+
+### Installation
+
+You can install the development version of the package ‘rcalpuff’ from
+the repository at [GitHub](https://github.com/maxbre/rcalpuff/) with:
+
+``` r
+if(!require("devtools")) install.packages("devtools")
+devtools::install_github("maxbre/rcalpuff")
+```
