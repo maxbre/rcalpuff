6c10e20e7b934fc2e46fe13dd32407ce2ae57723
maxbre <maxbre67@gmail.com>
2022-05-13 13:51:54 +0200

    param transf_factor in mapview_calpost_grd

--- R/mapview_calpost_grd.R
+++ R/mapview_calpost_grd.R
@@ -1,14 +1,15 @@
 #' mapview_calpost_grd
 #'
-#' Create a mapview object from Calpost grid file and export as html and png files
+#' Create a mapview object from CALPOST grid file and export as html and png files
 #'
-#' @param file_grd string path to the Calpost grid file
+#' @param file_grd string path to the CALPOST grid file
 #' @param epsg number epsg to set crs in the raster object
 #' @param at numeric vector defining breakpoints for the visualization,
 #'        default value is NULL by using continuous 'pretty' breakpoints, see lattice::levelplot function for details
 #' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
-#' @param string_filename string to name the output files
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
+#' @param trans_factor number, transformation factor to be applied to calculated values, leave it as default = 1
 #' @return mapview object exported as html and png files
 #' @export
 
@@ -16,10 +17,15 @@ mapview_calpost_grd <- function(file_grd,
                                 epsg = 32632,
                                 at = NULL,
                                 export = FALSE,
-                                string_filename = ' file_name',
-                                name_of_map_layer = 'layer_name'){
+                                name_of_map_layer = 'mapview_layer_name',
+                                string_filename = 'export_filename',
+                                trans_factor = 1){
 
+  # read grd fle
   grd <- read_calpost_grd(file_grd, epsg)
+
+  #eventually apply a transf_factor
+  grd <- grd * trans_factor
 
   # eventually to hardcode the binning in mapview
   #my_bins<-c(0, 1, 2, 3, 4, 5, 10, 100, round(max(raster::values(grd)),0))
