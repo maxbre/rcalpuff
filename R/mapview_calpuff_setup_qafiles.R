5abcb903d7b4d45fa33d490346821a7843688930
maxbre <maxbre67@gmail.com>
2022-05-17 09:47:09 +0000

    setup qafiles calpuff

--- R/mapview_calpuff_setup_qafiles.R
+++ R/mapview_calpuff_setup_qafiles.R
@@ -1,153 +1,230 @@
-#' mapview_met_domain
+#' Mapview CALPUFF meteorological domain
 #'
-#' mapview Calpuff setup meteorological domain
+#' Create a mapview object from CALPUFF setup meteorological domain file 'qagrid.bna' and eventually export it as html and png files
 #'
-#' @param qagrid.bna string path to Calpuff setup file qagrid.bna
+#' @param qagrid.bna string path to Calpuff setup file 'qagrid.bna'
 #' @param epsg number epsg to set crs of the file domain
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_met_domain <- function(qagrid.bna, epsg = 32632, string_filename = 'file_name_met_domain', name_of_map_layer = 'layer_name_met_domain'){
+mapview_met_domain <- function(qagrid.bna,
+                               epsg = 32632,
+                               export = FALSE,
+                               string_filename = 'file_name_met_domain',
+                               name_of_map_layer = 'layer_name_met_domain'){
+
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
 
   domain <- read_qagrid_bna(qagrid.bna, epsg = epsg, skip = 1, n_max = 5)
 
   # mapview
   map <- mapview::mapview(domain, color="green", layer.name = name_of_map_layer)
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
+
+  map
 
 }
 
-#' mapview_comp_domain
+#' Mapview CALPUFF computational domain
 #'
-#' mapview Calpuff setup computational domain
+#' Create a mapview object from CALPUFF setup computational domain file 'qagrid.bna' and eventually export it as html and png files
 #'
-#' @param qagrid.bna string path to Calpuff setup file qagrid.bna
+#' @param qagrid.bna string path to Calpuff setup file 'qagrid.bna'
 #' @param epsg number epsg to set crs of the file domain
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_comp_domain <- function(qagrid.bna, epsg = 32632, string_filename = 'file_name_comp_domain', name_of_map_layer = 'layer_name_comp_domain'){
+mapview_comp_domain <- function(qagrid.bna,
+                                epsg = 32632,
+                                export = FALSE,
+                                string_filename = 'file_name_comp_domain',
+                                name_of_map_layer = 'layer_name_comp_domain'){
+
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
 
   domain <- read_qagrid_bna(qagrid.bna, epsg = epsg, skip = 7, n_max = 5)
 
   # mapview
   map <- mapview::mapview(domain, color="yellow", layer.name = name_of_map_layer)
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
 
+  map
 }
 
-#' mapview_samp_domain
+#' Mapview CALPUFF sampling domain
 #'
-#' mapview Calpuff setup sampling domain
+#' Create a mapview object from CALPUFF setup sampling domain file 'qagrid.bna' and eventually export it as html and png files
 #'
 #' @param qagrid.bna string path to Calpuff setup file qagrid.bna
 #' @param epsg number epsg to set crs of the file domain
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_samp_domain <- function(qagrid.bna, epsg = 32632, string_filename = 'file_name_samp_domain', name_of_map_layer = 'layer_name_samp_domain'){
+mapview_samp_domain <- function(qagrid.bna,
+                                epsg = 32632,
+                                export = FALSE,
+                                string_filename = 'file_name_samp_domain',
+                                name_of_map_layer = 'layer_name_samp_domain'){
+
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
 
   domain <- read_qagrid_bna(qagrid.bna, epsg = epsg, skip = 13, n_max = 5)
 
   # mapview
   map <- mapview::mapview(domain, color="red", layer.name = name_of_map_layer)
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
 
+  map
 }
 
-#' mapview_emi_points
+#' Mapview CALPUFF emission points
 #'
-#' mapview Calpuff setup emission points
+#' Create a mapview object from CALPUFF setup emission points file 'qapnts.dat' and eventually export it as html and png files
 #'
-#' @param qapnts.dat string path to Calpuff setup file emission points qapnts.dat
+#' @param qapnts.dat string path to Calpuff setup file emission points 'qapnts.dat'
 #' @param epsg number epsg to set crs of the emission points
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_emi_points <- function(qapnts.dat, epsg = 32632, string_filename = 'file_name_emi_points', name_of_map_layer = 'layer_name_emi_points'){
+mapview_emi_points <- function(qapnts.dat,
+                               epsg = 32632,
+                               export = FALSE,
+                               string_filename = 'file_name_emi_points',
+                               name_of_map_layer = 'layer_name_emi_points'){
+
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
 
   emi_pts_sf <- read_qapnts_dat(qapnts.dat, epsg = epsg)
 
   # mapview
   map <- mapview::mapview(emi_pts_sf, layer.name = name_of_map_layer)
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
 
+  map
 }
 
-#' mapview_disc_recs
+#' Mapview CALPUFF discrete receptors
 #'
-#' mapview Calpuff setup discrete receptors
+#' Create a mapview object from CALPUFF setup discrete receptors file 'qarecd.dat' and eventually export it as html and png files
 #'
-#' @param qarecd.dat string path to Calpuff setup file discrete receptors qarecd.dat
+#' @param qarecd.dat string path to Calpuff setup file discrete receptors 'qarecd.dat'
 #' @param epsg number epsg to set crs of the discrete receptors
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_disc_recs <- function(qarecd.dat, epsg = 32632, string_filename = 'file_name_disc_rescs', name_of_map_layer = 'layer_name_disc_recs'){
+mapview_disc_recs <- function(qarecd.dat,
+                              epsg = 32632,
+                              export = FALSE,
+                              string_filename = 'file_name_disc_rescs',
+                              name_of_map_layer = 'layer_name_disc_recs'){
 
-  disc_recs_sf <- read_qarecd_dat(qarecd.dat, epsg = epsg)
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
+
+  # note here suppressing warning of parsing failure number of columns expected
+  # it is something related to the function read_qarecd.dat
+  disc_recs_sf <- suppressWarnings(read_qarecd_dat(qarecd.dat, epsg = epsg))
 
   # mapview
   map <- mapview::mapview(disc_recs_sf, layer.name = name_of_map_layer)
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
 
+  map
 }
 
-#' mapview_grid_recs
+#' Mapview CALPUFF grid receptors
 #'
-#' mapview Calpuff setup grid receptors
+#' Create a mapview object from CALPUFF setup grid receptors file 'qarecg.dat' and eventually export it as html and png files
 #'
-#' @param qarecg.dat string path to Calpuff setup file grid receptors qareg.dat
+#' @param qarecg.dat string path to Calpuff setup file grid receptors 'qarecg.dat'
 #' @param epsg number epsg to set crs of the grid receptors
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_grid_recs <- function(qarecg.dat, epsg = 32632, string_filename = 'file_name_grid_rescs', name_of_map_layer = 'layer_name_grid_recs'){
+mapview_grid_recs <- function(qarecg.dat,
+                              epsg = 32632,
+                              export = FALSE,
+                              string_filename = 'file_name_grid_rescs',
+                              name_of_map_layer = 'layer_name_grid_recs'){
 
-  grid_recs_sf <- read_qarecg_dat(qarecg.dat, epsg = epsg)
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
+
+  # note here suppressing warning of parsing failure number of columns expected
+  # it is something related to the function read_qarecg.dat
+  grid_recs_sf <- suppressWarnings(read_qarecg_dat(qarecg.dat, epsg = epsg))
 
   # mapview
   map <- mapview::mapview(grid_recs_sf, layer.name = name_of_map_layer)
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
 
+  map
 }
 
-#' mapview_qaterr_grd
+#' Mapview CALPUFF terrain
 #'
-#' Create a mapview object from Calpuff setup grid file qaterr.grd and export as html and png files
+#' Create a mapview object from CALPUFF setup grid file 'qaterr.grd' and eventually export it as html and png files
 #'
-#' @param file_grd string path to the Calpuff setup grid file qaterr.grd
+#' @param qaterr.grd string path to the Calpuff setup grid file 'qaterr.grd'
 #' @param epsg number epsg to set crs in the raster object
-#' @param string_filename string to name the output files
+#' @param export logical, export mapview object as hmtl and png files? The default value is equal to FALSE
 #' @param name_of_map_layer string to name the mapview layer
+#' @param string_filename string to name the output files
 #' @return mapview object exported as html and png files
 #' @export
 
-mapview_calpuff_terr <- function(file_grd, epsg = 32632, string_filename = ' file_name_grid_terr', name_of_map_layer = 'layer_name_grid_terr'){
+mapview_terr_grd <- function(qaterr.grd,
+                                 epsg = 32632,
+                                 export = FALSE,
+                                 name_of_map_layer = 'layer_name_grid_terr',
+                                 string_filename = ' file_name_grid_terr'){
 
-  grd <- read_qaterr_grd(file_grd, epsg)
+  # get rid of mapview warning message
+  options(rgdal_show_exportToProj4_warnings = "none")
+
+  # read grid
+  grd <- read_qaterr_grd(qaterr.grd, epsg)
 
   # eventually to hardcode the binning in mapview
   #my_bins<-c(0, 1, 2, 3, 4, 5, 10, 100, round(max(raster::values(grd)),0))
   #my_bins<-pretty(range(raster::values(grd)))
+
+  # get rid of mapview warning message
+  #options(rgdal_show_exportToProj4_warnings = "none")
 
   # mapview
   map <- mapview::mapview(grd,
@@ -161,7 +238,10 @@ mapview_calpuff_terr <- function(file_grd, epsg = 32632, string_filename = ' fil
                           layer.name = name_of_map_layer)
 
 
-  rfunctions::export_mapview(map, string_filename)
+  # eventually export the map to hml and png
+  if(export) rfunctions::export_mapview(map, string_filename)
+
+  map
 
 }
 
